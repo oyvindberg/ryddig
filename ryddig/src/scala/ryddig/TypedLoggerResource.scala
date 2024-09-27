d9354545aa841c199e35dec6a0f57b3aea13aabb
Øyvind Raddum Berg <elacin@gmail.com>
2024-09-27 22:45:51 +0200

    cleanup resource

--- ryddig/src/scala/ryddig/TypedLoggerResource.scala
+++ ryddig/src/scala/ryddig/TypedLoggerResource.scala
@@ -3,69 +3,68 @@ package ryddig
 import java.io.Flushable
 
 trait TypedLoggerResource[U] {
-  def use[T](f: TypedLogger[U] => T): T
+  def acquire(): (TypedLogger[U], () => Unit)
 
-  final def unsafeGet(): TypedLogger[U] = {
-    var ret: TypedLogger[U] = null
-    use(ret = _)
-    ret
+  final def use[T](f: TypedLogger[U] => T): T = {
+    val (logger, release) = acquire()
+    try f(logger)
+    finally release()
   }
 }
 
 object TypedLoggerResource {
   final implicit class Ops[U1](private val one: TypedLoggerResource[U1]) extends AnyVal {
     def map[U2](transform: TypedLogger[U1] => TypedLogger[U2]): TypedLoggerResource[U2] =
-      new TypedLoggerResource[U2] {
-        override def use[T](f: TypedLogger[U2] => T): T =
-          one.use(logger => f(transform(logger)))
+      () => {
+        val (logger, release) = one.acquire()
+        (transform(logger), release)
       }
 
     def zipWith[U2](other: TypedLoggerResource[U2]): TypedLoggerResource[(U1, U2)] =
-      new TypedLoggerResource[(U1, U2)] {
-        override def use[T](f: TypedLogger[(U1, U2)] => T): T =
-          one.use(oneLogger => other.use(otherLogger => f(oneLogger.zipWith(otherLogger))))
+      () => {
+        val (logger1, release1) = one.acquire()
+        val (logger2, release2) = other.acquire()
+        val releaseBoth: () => Unit = () => {
+          release1()
+          release2()
+        }
+        (logger1.zipWith(logger2), releaseBoth)
       }
 
     def maybeZipWith[U2](other: Option[TypedLoggerResource[U2]]): TypedLoggerResource[(U1, Option[U2])] =
-      new TypedLoggerResource[(U1, Option[U2])] {
-        override def use[T](f: TypedLogger[(U1, Option[U2])] => T): T =
-          one.use { oneLogger =>
-            other match {
-              case Some(other) => other.use(otherLogger => f(oneLogger.maybeZipWith(Some(otherLogger))))
-              case None        => f(oneLogger.maybeZipWith(None))
+      () => {
+        val (logger1, release1) = one.acquire()
+        other match {
+          case None =>
+            (logger1.maybeZipWith(None), release1)
+          case Some(other) =>
+            val (logger2, release2) = other.acquire()
+            val releaseBoth = () => {
+              release1()
+              release2()
             }
-          }
+            (logger1.maybeZipWith(Some(logger2)), releaseBoth)
+        }
       }
 
-    def untyped: LoggerResource = new TypedLoggerResource[Unit] {
-      override def use[T](f: TypedLogger[Unit] => T): T = one.use(l => f(l.untyped))
+    def untyped: LoggerResource = () => {
+      val (logger, release) = one.acquire()
+      (logger.untyped, release)
     }
   }
 
   def pure[U](logger: TypedLogger[U]): TypedLoggerResource[U] =
-    new TypedLoggerResource[U] {
-      override def use[T](f: TypedLogger[U] => T): T = f(logger)
-    }
+    () => (logger, () => ())
 
   def autoCloseable[U <: AutoCloseable](mkLogger: => TypedLogger[U]): TypedLoggerResource[U] =
-    new TypedLoggerResource[U] {
-      override def use[T](f: TypedLogger[U] => T): T = {
-        var inited: TypedLogger[U] = null
-        try {
-          inited = mkLogger
-          f(inited)
-        } finally if (inited != null) inited.underlying.close()
-      }
+    () => {
+      val logger = mkLogger
+      (logger, () => logger.underlying.close())
     }
 
   def flushable[U <: Flushable](mkLogger: => TypedLogger[U]): TypedLoggerResource[U] =
-    new TypedLoggerResource[U] {
-      override def use[T](f: TypedLogger[U] => T): T = {
-        var inited: TypedLogger[U] = null
-        try {
-          inited = mkLogger
-          f(inited)
-        } finally if (inited != null) inited.underlying.flush()
-      }
+    () => {
+      val logger = mkLogger
+      (logger, () => logger.underlying.flush())
     }
 }
