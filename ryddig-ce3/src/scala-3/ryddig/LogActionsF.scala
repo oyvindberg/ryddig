e063d9d648b39c53c9328f22cde389053f34a44d
Øyvind Raddum Berg <elacin@gmail.com>
2024-10-02 00:02:18 +0200

    let's hope this elides `Metadata` allocations

--- ryddig-ce3/src/scala-3/ryddig/LogActionsF.scala
+++ ryddig-ce3/src/scala-3/ryddig/LogActionsF.scala
@@ -10,36 +10,36 @@ trait LogActionsF[F[_]] extends Any {
 
   def unit: F[Unit]
 
-  private inline def doLog[T: Formatter](t: => T, th: Option[Throwable], metadata: Metadata): F[Unit] =
-    if metadata.logLevel >= minLogLevel then self(t, th, metadata) else unit
+  private inline def doLog[T: Formatter](logLevel: LogLevel, t: => T, th: Option[Throwable], metadata: Metadata): F[Unit] =
+    if logLevel >= minLogLevel then self(t, th, metadata) else unit
 
   final inline def log[T: Formatter](logLevel: LogLevel, t: => T)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, None, new Metadata(Instant.now, logLevel, l, f, e))
+    doLog(logLevel, t, None, new Metadata(Instant.now, logLevel, l, f, e))
 
   final inline def log[T: Formatter](logLevel: LogLevel, t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, Some(th), new Metadata(Instant.now, logLevel, l, f, e))
+    doLog(logLevel, t, Some(th), new Metadata(Instant.now, logLevel, l, f, e))
 
   final inline def debug[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.debug, l, f, e))
+    doLog(LogLevel.debug, t, None, new Metadata(Instant.now, LogLevel.debug, l, f, e))
 
   final inline def debug[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.debug, l, f, e))
+    doLog(LogLevel.debug, t, Some(th), new Metadata(Instant.now, LogLevel.debug, l, f, e))
 
   final inline def info[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.info, l, f, e))
+    doLog(LogLevel.info, t, None, new Metadata(Instant.now, LogLevel.info, l, f, e))
 
   final inline def info[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.info, l, f, e))
+    doLog(LogLevel.info, t, Some(th), new Metadata(Instant.now, LogLevel.info, l, f, e))
 
   final inline def warn[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.warn, l, f, e))
+    doLog(LogLevel.warn, t, None, new Metadata(Instant.now, LogLevel.warn, l, f, e))
 
   final inline def warn[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.warn, l, f, e))
+    doLog(LogLevel.warn, t, Some(th), new Metadata(Instant.now, LogLevel.warn, l, f, e))
 
   final inline def error[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.error, l, f, e))
+    doLog(LogLevel.error, t, None, new Metadata(Instant.now, LogLevel.error, l, f, e))
 
   final inline def error[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): F[Unit] =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.error, l, f, e))
+    doLog(LogLevel.error, t, Some(th), new Metadata(Instant.now, LogLevel.error, l, f, e))
 }
--- ryddig/src/scala-3/ryddig/LogActions.scala
+++ ryddig/src/scala-3/ryddig/LogActions.scala
@@ -6,36 +6,36 @@ import java.time.Instant
 import scala.Ordering.Implicits.given
 
 trait LogActions extends Any { self: Logger =>
-  private inline def doLog[T: Formatter](t: => T, th: Option[Throwable], metadata: Metadata): Unit =
-    if metadata.logLevel >= minLogLevel then self(t, th, metadata)
+  private inline def doLog[T: Formatter](logLevel: LogLevel, t: => T, th: Option[Throwable], metadata: Metadata): Unit =
+    if logLevel >= minLogLevel then self(t, th, metadata)
 
   final inline def log[T: Formatter](logLevel: LogLevel, t: => T)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, None, new Metadata(Instant.now, logLevel, l, f, e))
+    doLog(logLevel, t, None, new Metadata(Instant.now, logLevel, l, f, e))
 
   final inline def log[T: Formatter](logLevel: LogLevel, t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, Some(th), new Metadata(Instant.now, logLevel, l, f, e))
+    doLog(logLevel, t, Some(th), new Metadata(Instant.now, logLevel, l, f, e))
 
   final inline def debug[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.debug, l, f, e))
+    doLog(LogLevel.debug, t, None, new Metadata(Instant.now, LogLevel.debug, l, f, e))
 
   final inline def debug[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.debug, l, f, e))
+    doLog(LogLevel.debug, t, Some(th), new Metadata(Instant.now, LogLevel.debug, l, f, e))
 
   final inline def info[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.info, l, f, e))
+    doLog(LogLevel.info, t, None, new Metadata(Instant.now, LogLevel.info, l, f, e))
 
   final inline def info[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.info, l, f, e))
+    doLog(LogLevel.info, t, Some(th), new Metadata(Instant.now, LogLevel.info, l, f, e))
 
   final inline def warn[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.warn, l, f, e))
+    doLog(LogLevel.warn, t, None, new Metadata(Instant.now, LogLevel.warn, l, f, e))
 
   final inline def warn[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.warn, l, f, e))
+    doLog(LogLevel.warn, t, Some(th), new Metadata(Instant.now, LogLevel.warn, l, f, e))
 
   final inline def error[T: Formatter](t: => T)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, None, new Metadata(Instant.now, LogLevel.error, l, f, e))
+    doLog(LogLevel.error, t, None, new Metadata(Instant.now, LogLevel.error, l, f, e))
 
   final inline def error[T: Formatter](t: => T, th: Throwable)(using l: Line, f: File, e: Enclosing): Unit =
-    doLog(t, Some(th), new Metadata(Instant.now, LogLevel.error, l, f, e))
+    doLog(LogLevel.error, t, Some(th), new Metadata(Instant.now, LogLevel.error, l, f, e))
 }
